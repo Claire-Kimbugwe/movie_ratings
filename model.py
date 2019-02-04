984a423c67285245349cce7e39d229db663b621c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-04 13:05:35 -0800

    all

--- model.py
+++ model.py
@@ -23,12 +23,36 @@ class User(db.Model):
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "User user_id={} email={}".format(self.user_id,self.email)
+
 
 # Put your Movie and Rating model classes here.
 
+class Movie(db.Model):
+    """Movie data."""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    released_at = db.Column(db.DateTime, nullable=False)
+    imdb_url = db.Column(db.String(200), nullable=False)
+
+
+class Ratings(db.Model):
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, primary_key = True , autoincrement = True)
+    movie_id = db.Column(db.Integer,nullable= False)
+    user_id = db.Column(db.Integer, nullable= False)
+    score= db.Column(db.Integer, nullable=False)
 
 ##############################################################################
 # Helper functions
+
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
